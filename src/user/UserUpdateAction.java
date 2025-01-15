2f3aee74e0a5026f4df962cd591de8e6d6eec2df
takekubo <takekubo@LAPTOP-H1D53K7T>
2025-01-15 12:21:00 +0900

    途中経過

--- src/user/UserUpdateAction.java
+++ src/user/UserUpdateAction.java
@@ -18,15 +18,31 @@ public class UserUpdateAction extends Action{
 		HttpSession session = req.getSession();
 
 		Integer userID = (Integer) session.getAttribute("userID");
-
-		System.out.println(userID);
-
-
-
 		user = userDao.get(userID);
 		req.setAttribute("user", user);
 
-		req.getRequestDispatcher("user_update.jsp").forward(req,res);
+
+		 String actionParam = req.getParameter("action");
+	        if (actionParam != null) {
+	            int action = Integer.parseInt(actionParam);
+
+	            // 条件に応じてリダイレクト先を変更
+	            switch (action) {
+	                case 1:
+	                    req.getRequestDispatcher("email_update.jsp").forward(req, res);
+	                    break;
+	                case 2:
+	                    req.getRequestDispatcher("password_update.jsp").forward(req, res);
+	                    break;
+	                default:
+	                    // 無効な値の場合、デフォルト画面（例: ユーザー情報更新画面）
+	                    req.getRequestDispatcher("user_update.jsp").forward(req, res);
+	                    break;
+	            }
+	        } else {
+	            // "action" パラメータが指定されていない場合、デフォルト画面
+	            req.getRequestDispatcher("user_update.jsp").forward(req, res);
+	        }
 
 	}
 }
